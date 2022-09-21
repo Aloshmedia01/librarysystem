adad39a513de9a19f7fde976e7fb4070acddb478
Aloshmedia01 <abrahamakanmu6@gmail.com>
2022-09-22 00:55:28 +0100

    first commits

---
+++ about.php
@@ -0,0 +1,212 @@
+      
+        <!-- Start About Us Section -->
+    <section id="about-section" class="about-section">
+        <div class="container">
+            <div class="row">
+               <div class="col-md-5">
+                   <div class="about-img">
+                       <img src="asset/images/corporate1.jpg" class="img-responsive" alt="About images">
+                       <div class="head-text">
+                           <p>Building a comprehensive digital library system will help users to manage all phases of the information lifecycle. Of particular importance.</p>
+                           <span>VICTOR OGBU</span>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-md-7">
+                   <div class="about-text">
+                       <p>Libraries have been an important part of the educational and information sector of any institution.  The success of any library largely depends on proper management. Several libraries have suffered failure as a result of inadequate management and incapacitations in handling sensitive information as regards members of the library. This research, therefore, is aimed at developing a digital library system, which will help direct and position the library to meet its ever-increasing demands. In the course of the development of this new system, the current system was analytically and critically studied or assessed, and thus the identified strengths and weaknesses were highlighted and a new system was designed for the weakness. The present system was analyzed using a standard procedure recognized worldwide for such purpose and this is called Structured System Analysis and Design Methodology (SSADM).  The programming language was used to code the program modules developed for the system using some computer-aided design tools.</p>
+
+                   </div>
+                   
+                   <div class="about-list">
+                       <h4>Some important Feature</h4>
+                       <ul>
+                           <li><i class="fa fa-check-square"></i>BORROW BOOK</li>
+                           <li><i class="fa fa-check-square"></i>RETURN BOOK.</li>
+                           <li><i class="fa fa-check-square"></i>SEARCH BOOK.</li>
+ 
+                       </ul>
+                       
+                      
+                   </div>
+                   
+               </div>
+                
+                
+                
+            </div>
+        </div>
+    </section>
+        
+        <!-- Start About-section 2 -->
+        <section id="about-section-2">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                   <div class="about-text">
+                       <p>.</p>
+                       <p></p>
+                   </div>
+                   
+                   <div class="skill-shortcode">
+                        
+                        <!-- Progress Bar -->
+                        <div class="skill">
+                            <p>Web Design</p>          
+                            <div class="progress">         
+                                <div class="progress-bar" role="progressbar"  data-percentage="60">
+                                    <span class="progress-bar-span" >60%</span>
+                                    <span class="sr-only">60% Complete</span>
+                                </div>
+                            </div>  
+                        </div>
+                        
+                        <!-- Progress Bar -->
+                        <div class="skill">
+                            <p>HTML & CSS</p>          
+                            <div class="progress">         
+                                <div class="progress-bar" role="progressbar"  data-percentage="95">
+                                    <span class="progress-bar-span" >95%</span>
+                                    <span class="sr-only">95% Complete</span>
+                                </div>
+                            </div>  
+                        </div>
+                        
+                        <!-- Progress Bar -->
+                        <div class="skill">
+                            <p>Wordpress</p>          
+                            <div class="progress">         
+                                <div class="progress-bar" role="progressbar"  data-percentage="80">
+                                    <span class="progress-bar-span" >80%</span>
+                                    <span class="sr-only">80% Complete</span>
+                                </div>
+                            </div>  
+                        </div>
+                        
+                        <!-- Progress Bar -->
+                        <div class="skill">
+                            <p>Joomla</p>          
+                            <div class="progress">         
+                                <div class="progress-bar" role="progressbar"  data-percentage="100">
+                                    <span class="progress-bar-span" >100%</span>
+                                    <span class="sr-only">100% Complete</span>
+                                </div>
+                            </div>  
+                        </div>
+                        
+                        <!-- Progress Bar -->
+                        <div class="skill">
+                            <p>Extension</p>          
+                            <div class="progress">         
+                                <div class="progress-bar" role="progressbar"  data-percentage="70">
+                                    <span class="progress-bar-span" >70%</span>
+                                    <span class="sr-only">70% Complete</span>
+                                </div>
+                            </div>  
+                        </div>
+                                                            
+                    </div>
+                   
+               </div>
+                <div class="col-md-6 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="300ms">
+                    
+                    <!-- Start Accordion Section -->
+                    <div class="panel-group" id="accordion">
+
+                        <!-- Start Accordion 1 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-1">
+                                        <i class="fa fa-angle-left control-icon"></i> Who We are
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-1" class="panel-collapse collapse in">
+                                <div class="panel-body">.</div>								</div>
+                        </div>
+                        <!-- End Accordion 1 -->
+
+                        <!-- Start Accordion 2 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-2" class="collapsed">
+                                        <i class="fa fa-angle-left control-icon"></i> What we do
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-2" class="panel-collapse collapse">
+                                <div class="panel-body"></div>								</div>
+                        </div>
+                        <!-- End Accordion 2 -->
+                        
+                        <!-- Start Accordion 3 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-3" class="collapsed">
+                                        <i class="fa fa-angle-left control-icon"></i> Our Goal
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-3" class="panel-collapse collapse">
+                                <div class="panel-body">.</div>								</div>
+                        </div>
+                        <!-- End Accordion 3 -->
+
+                        <!-- Start Accordion 4 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-4" class="collapsed">
+                                        <i class="fa fa-angle-left control-icon"></i> Why Choose Us ?
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-4" class="panel-collapse collapse">
+                                <div class="panel-body">.</div>
+                            </div>
+                        </div>
+                        <!-- End Accordion 4 -->
+
+                        <!-- Start Accordion 5 -->
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse-5" class="collapsed">
+                                        <i class="fa fa-angle-left control-icon"></i> Our Great Support
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-5" class="panel-collapse collapse">
+                                <div class="panel-body">.</div>
+                            </div>
+                        </div>
+                        <!-- End Accordion 5 -->
+
+                    </div>
+                    <!-- End Accordion section -->
+                    
+                </div><!--/.col-md-6 -->
+                </div>
+            </div>
+        </section>
+        <!-- Start About-section 2 -->
+        
+        
+        <!-- Start Call to Action Section -->
+    <section class="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 wow zoomIn" data-wow-duration="2s" data-wow-delay="300ms">
+                    <p>We have available books which includes
+<br> ICT, History, Business Administrative, Public Administrative, science related,<br>The library is open to help you explore</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Call to Action Section -->
+        
+        
+        
---
+++ home.php
@@ -0,0 +1,214 @@
+       
+        <!-- Start Header Section -->
+        <div class="banner">
+            <div class="overlay">
+                <div class="container">
+                    <div class="intro-text">
+                        <h1>Welcome To The <span>FEDERAL POLYTECHNIC DIGITAL LIBRARY</span></h1>
+                        <p>Explore and Borrow Books <br> Through Online</p> 
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Header Section -->
+        
+        
+        <!-- Start About Us Section -->
+    <section id="about-section" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                        <h2>About Us</h2>
+                    </div>                        
+                </div>
+            </div>
+            <div class="row">
+               <div class="col-md-5">
+                   <div class="about-img">
+                       <img src="asset/images/corporate1.jpg" class="img-responsive" alt="About images">
+                       <div class="head-text">
+                           <p>Digital Library System (DLS) extends and integrates approaches adopted in traditional libraries as well as in distributed information systems, to yield high-end information systems, services, and institutions. Some of the parts or components of digital libraries and discussion on several developments in this emerging field were explored.</p>
+                           <span>VICTOR OGBU</span>
+                       </div>
+                   </div>
+               </div>
+               <div class="col-md-7">
+                   <div class="about-text">
+                       <p>Libraries have been an important part of the educational and information sector of any institution.  The success of any library largely depends on proper management. Several libraries have suffered failure as a result of inadequate management and incapacitations in handling sensitive information as regards members of the library. This research, therefore, is aimed at developing a digital library system, which will help direct and position the library to meet its ever-increasing demands. In the course of the development of this new system, the current system was analytically and critically studied or assessed, and thus the identified strengths and weaknesses were highlighted and a new system was designed for the weakness. The present system was analyzed using a standard procedure recognized worldwide for such purpose and this is called Structured System Analysis and Design Methodology (SSADM).  The programming language was used to code the program modules developed for the system using some computer-aided design tools..</p>
+                       
+                   </div>
+                   
+                 <div class="about-list">
+                       <h4>Some important Feature</h4>
+                       <ul>
+                           <li><i class="fa fa-check-square"></i>BORROW BOOK.</li>
+                           <li><i class="fa fa-check-square"></i>RETURNED BOOK.</li>
+                           <li><i class="fa fa-check-square"></i>FILTER BOOK</li>
+                         
+                       </ul>
+                       
+                      
+                   </div>
+                   
+               </div>
+                
+                
+                
+            </div>
+        </div>
+    </section>
+        
+        
+        <!-- Start Call to Action Section -->
+    <section class="call-to-action">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 wow zoomIn" data-wow-duration="2s" data-wow-delay="300ms">
+                    <p> We have available books which includes<br> ICT, History, Business Administrative, Public Administrative, science related,  <br>The library is open to help you explore</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Call to Action Section -->
+        
+        
+        
+        
+        <!-- Start Team Member Section -->
+    <section id="team-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                        <h2>Our Team</h2>
+                        <p>GROUP 59 MEMBERS</p>
+                    </div>                        
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="300ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-1.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>VICTOR OGBU</h4>
+                            <p>MEMEBER OF GROUP 1</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="600ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-2.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>WILLIAMS DAVID</h4>
+                            <p>MEMBER</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="900ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-3.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>HELEN CHUKWUEMEKA</h4>
+                            <p>MEMEBER</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+                <div class="col-md-3 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="1200ms">
+                    <div class="team-member">
+                        <img src="asset/images/team/team-4.jpg" class="img-responsive" alt="">
+                        <div class="team-details">
+                            <h4>DAVID JOHN</h4>
+                            <p>MEMEBER</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div><!-- /.col-md-3 -->
+            </div>
+        </div>
+    </section>
+    <!-- End Team Member Section -->
+        
+     
+        
+        <!-- Start Service Section -->
+        <section id="service-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title text-center wow fadeInDown" data-wow-duration="2s" data-wow-delay="50ms">
+                            <h2>Our Books</h2>
+                            <p>Available Books in the Library</p>
+                        </div>                        
+                    </div>
+                </div>
+
+                        <!-- Start Service Section -->
+     
+                        <?php
+                    // `AccessionNo`, `BookTitle`, `BookDesc`, `Author`, `PublishDate`, `BookPublisher`, `Category`, `BookPrice`, `BookQuantity`, `Status`, `BookType`, `DeweyDecimal`, `OverAllQty`, `Donate`, `Remarks`
+                    
+                            // $mydb->setQuery("SELECT *, sum(BookQuantity) as qty FROM `tblbooks` GROUP BY BookTitle");
+                    $mydb->setQuery("SELECT * FROM `tblbooks` WHERE Status='Available' GROUP BY BookTitle"); 
+                            $cur = $mydb->loadResultlist();
+                            foreach ($cur as $result) {
+                                echo '<tr>'; 
+                                // echo '<td ><input type="checkbox" name="selector[]" id="selector[]" value="'.$result->SUBJ_ID. '"/>
+                                //      <a href="index.php?view=edit&id='.$result->SUBJ_ID.'">' . $result->SUBJ_CODE.'</a></td>';
+                                // echo '<td >' . $result->IBSN.'</td>';
+                                // echo '<td >'. $result->BookTitle.'</td>';
+                                // echo '<td>'.  $result->BookDesc.'</td>'; 
+                                // echo '<td>'. $result->Category.'</td>'; 
+                                // echo '<td>'. $result->Author.'</td>';
+                                // echo '<td>'. $result->BookPrice.'</td>';
+                                // echo '<td>'. $result->Status.'</td>';
+                                // echo '<td>'. $result->qty.'</td>';
+
+                                echo '<div class="col-md-3">
+                                            <div class="services-post">
+                                                <a href="index.php?q=borrow&id='.$result->IBSN.'"><i class="fa fa-book"></i></a>
+                                                <h2>'. $result->BookTitle.'</h2>
+                                                <p>'.  $result->BookDesc.'</p>
+                                            </div>
+                                        </div>';
+ 
+                      } 
+                    ?>
+
+
+                 
+                     
+                 
+            </div>
+        </section>
+        <!-- Start Service Section -->
+        
+        
+ 
